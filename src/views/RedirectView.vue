d960e59bba051ec29c8f127cdacbecd9e36bf575
Janosch Schwalm <janosch.schwalm@eb.de>
2022-05-22 12:03:12 +0200

    add redirect view

---
+++ src/views/RedirectView.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="auto">
+        hier ist der link für dich:
+        <a :href="$route.query.location">{{ $route.query.location }}</a>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
