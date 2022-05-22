f0d9d7b9bacf9b33385c5cc380bc62bebdea2605
CI <CI@example.com>
2022-05-22 10:05:01 +0000

    deploy

---
+++ js/about.59ab4fe8.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkuri_redirect"]=self["webpackChunkuri_redirect"]||[]).push([[443],{9030:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});var n=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,t=e.$createElement,r=e._self._c||t;return r("div",{staticClass:"about"},[r("h1",[e._v("This is an about page")])])}],i=r(1001),s={},a=(0,i.Z)(s,n,u,!1,null,null,null),c=a.exports}}]);
+//# sourceMappingURL=about.59ab4fe8.js.map
